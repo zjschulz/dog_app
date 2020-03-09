29e245ff0d2dc19d6dfd0cb1cfa3572fce0955dd
zjschulz <zjschulz@gmail.com>
2020-03-09 18:53:00 -0400

    expanded volunteer index view

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -2,4 +2,11 @@ class Volunteer < ApplicationRecord
     has_many :walks
     has_many :dogs, through: :walks
     validates_presence_of :name
+
+    def time
+        self.walks.sum do |t|
+            t.duration
+        end
+    end
+
 end
