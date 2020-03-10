03ac63321040a7715425661f03c671a074ef6ee5
zjschulz <zjschulz@gmail.com>
2020-03-09 20:37:25 -0400

    more changes to walks controller

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -1,15 +1,22 @@
 class WalksController < ApplicationController
 
     def index
-        @volunteer = Volunteer.find_by(id: params[:volunteer_id])
+        pull_volunteer
     end
 
     def new
-        
+        pull_volunteer
     end
 
     def create
         
     end
+
+    private
+
+    def pull_volunteer
+        @volunteer ||= Volunteer.find_by(id: params[:volunteer_id])
+    end
+
 
 end
