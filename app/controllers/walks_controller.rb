8b6cdf3c9bae1a9a73d6979317a4a5f31b9e6d09
zjschulz <zjschulz@gmail.com>
2020-03-12 20:30:30 -0400

    improved authorization

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -1,24 +1,29 @@
 class WalksController < ApplicationController
     before_action :redirect_if_not_logged_in
-    before_action :validate_volunteer
+    before_action :pull_volunteer, only: [:index, :new]
 
     def index
-        pull_volunteer
+        redirect_if_invalid
     end
 
     def new
-        pull_volunteer
+        redirect_if_invalid
         @walk = Walk.new
     end
 
     def create
         @volunteer = Volunteer.find_by(id: params[:walk][:volunteer_id])
-        @walk = Walk.new(walk_params)
-        if @walk.save
-            redirect_to volunteer_walks_path(@walk.volunteer)
+        if user_authorized?
+            @walk = Walk.new(walk_params)
+            if @walk.save
+                redirect_to volunteer_walks_path(@walk.volunteer)
+            else
+                render :new
+            end
         else
-            render :new
+            redirect_to volunteers_path
         end
+
     end
 
     private
@@ -31,8 +36,14 @@ class WalksController < ApplicationController
         params.require(:walk).permit(:dog_id, :duration, :date, :volunteer_id)
     end
 
-    def validate_volunteer
-        redirect_to volunteers_path unless pull_volunteer
+    def user_authorized?
+        @volunteer.user && (@volunteer.user.id == current_user.id)
+    end
+
+    def redirect_if_invalid
+        if @volunteer.nil? || !user_authorized?
+            redirect_to volunteers_path
+        end
     end
 
 end
